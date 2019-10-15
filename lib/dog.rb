64570f40e9b79a95964c6cdcd0d70f4167325def
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-15 03:14:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+  def bark 
+    puts "Woof!"
+  end 
+  #sit method
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+  #person class
+class Person
+#   talk
+   def talk
+     puts "Hello World!"
+   end     
+#   #walk
+   def walk
+     puts "The Person is walking"
+   end   
+end
+
+  
+
+
+
+
+
+
